28327b303ba6873a91ea9ae7aeee090a7a4ac514
IfIdieTomorrow <chuck46@naver.com>
2020-11-22 22:12:02 +0900

    페이징 처리

--- routes/board/board.js
+++ routes/board/board.js
@@ -33,7 +33,7 @@ router.post("/write", (request, response)=>{
 router.get("/list", (request, response)=>{
     db.query("SELECT bno, title, writer, cont, hit, date_format(writedate,'%Y / %m / %d') FROM wf_board ORDER BY bno DESC",(err, boardList)=>{
         if(err) throw err;
-        response.render("boardList",{board:boardList})
+        response.redirect("/board/list/1");
     })
 });
 
@@ -96,47 +96,44 @@ router.delete("/delete", (request, response)=>{
     });
 });
 
-async function paging(curPage, totalRowCount) { // 페이지당 게시물 수 
-    let page_size = 10; // 보여지는 페이지의 갯수 : 1 ~ 5 페이지 
-    let page_list_size = 10; // limit 변수 (DB에서 가져올 게시물 수 no~ 
-    let no = 0; // 인자로 받은 현재 페이지 번호 
+//----------------------------------------------------------------------------------------------페이징 ----------------------------------------------------------------------------------------------------------
+router.get('/list/:page',(request,response)=>{
+    const page = request.params.page;
+    let sql = "select * from wf_board;";
+    db.query(sql, [], function(err, rows){
+        if(err) throw err;
+        db.query("select count(*) AS cnt from wf_board", function(err, rows){
+            if(err){ console.error('err',err); }
+            // console.log("rows", rows); //[{cnt:1}]
+            let cnt = rows[0].cnt;
+            let size = 10; // 보여줄 글의 수
+            let begin = (page - 1) * size; // 시작 글
+            let totalPage = Math.ceil(cnt/size);
+            let pageSize = 10; // 링크 갯수
+            let startPage = Math.floor((page-1)/pageSize)*pageSize+1;
+            let endPage = startPage + (pageSize - 1);
+            if(endPage > totalPage){
+                endPage = totalPage;
+            }
+            let max = cnt - ((page-1)*size);
+            db.query("select bno, title, writer, cont, hit, date_format(writedate, '%Y / %m / %d') from wf_board order by bno desc limit ?,?",
+            [begin, size], function(err, rows){
+                if(err) throw err;
+                // console.log('rows',rows);
+                const datas = {
+                    board: rows,
+                    page: page,
+                    pageSize: pageSize,
+                    startPage: startPage,
+                    endPage: endPage,
+                    totalPage: totalPage,
+                    max: max
+                };
+                response.render("boardList",{board:datas})
+            });
+        });
+    });
+});
 
-    curPage = Number(curPage) 
-    if (curPage <= 0) { 
-        no = 0; curPage = 1; 
-    } else{
-        no = (curPage - 1) * page_size; // 전체 페이지 갯수 
-    } 
-
-    if (totalRowCount < 0) totalRowCount = 0; 
-    let totalPage = Math.ceil(totalRowCount / page_size); // 전체 페이지 수 
-    if (totalPage < curPage) { 
-        no = 0; curPage = 1 // totalPage 보다 더 큰 curPage가 호출되면 에러 발생시키기 
-    } 
-
-    let startPage = ((curPage - 1) * page_list_size) + 1; // 시작 페이지 계산 
-    let endPage = (startPage + page_list_size) - 1; // 마지막 페이지 계산 
-
-    let result = { 
-        "curPage": curPage,
-        "page_list_size": page_list_size,
-        "page_size": page_size,
-        "totalPage": totalPage,
-        "startPage": startPage, 
-        "endPage": endPage, 
-        "no": no 
-    }
-    return result; 
-}
-
-//----------------------------------------------------------------------------------------------페이징 테스트 ----------------------------------------------------------------------------------------------------------
-// router.get('/list/:page',(request,response)=>{
-//     const page = request.params.page;
-//     db.query("SELECT COUNT(*) AS cnt FROM wf_board",(err, cnt)=>{
-//         if(err) throw err;
-//         let pageInfo = paging(page, cnt[0].cnt);
-//     });
-    
-// });
 
 module.exports = router;
